efd3b5cfb67e9bf1200a3d8ca6f0c72cad8f05a6
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-05-26 11:59:31 +0700

    Adding Constructor Object
    Menambah objek konstruktor untuk favoriteColor and favoriteResto

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+
+// Constructor for set
+function Favcolor(colors) {
+    this.favoriteColor = new Set(colors);
+    return Array.from(this.favoriteColor);
+}
+
+function Favresto(restos) {
+    this.favoriteRestaurant = new Set(restos);
+    return Array.from(this.favoriteRestaurant)
+}
+
 const firstUser = {};
 
 firstUser.name = "Monica";
 firstUser.gender ="Female";
 firstUser.age = 19;
 firstUser.email = "monica@dingdong.com";
-firstUser.favoriteColor = new Set([
-    "Yellow", "Pink", "White", "Purple",
+const monicaFavColor = new Favcolor([
+    "Yellow", "Pink", "White", "Purple",    
     "Yellow", "Pink", "White", "Purple"
-])
+]);
+firstUser.favoriteColor = monicaFavColor;
 firstUser.isHavePet = "Yes";
 firstUser.education = [
     {name: "SD 01", city: "Jakarta", graduate: 2016},
     {name: "SMP 02", city: "Jakarta", graduate: 2019},
     {name: "SMA 03", city: "Tangerang"}
 ]
-firstUser.favoriteRestaurant = new Set([
+const monicaFavResto = new Favresto([
     "Bento", "Sushi", "Pancake", "Eggy", 
     "Tempura", "Bento","Eggy", "Padang", 
     "Tteok", "Sushi", "Sushi"
-])
+]);
+firstUser.favoriteRestaurant = monicaFavResto;
 
 const secondUser = {};
 
@@ -29,10 +43,11 @@ secondUser.name = "Wendy";
 secondUser.gender ="Male";
 secondUser.age = 23;
 secondUser.email = "wendy@dingdong.com";
-secondUser.favoriteColor = new Set([
+const wendyFavColor = new Favcolor([
     "Blue", "Black", "Grey",
-    "Blue", "Black", "Grey"
+    "Blue", "Black", "Grey",
 ])
+secondUser.favoriteColor = wendyFavColor;
 secondUser.isHavePet = "No";
 secondUser.education = [
     {name: "SD 02", city: "Jakarta", graduate: 2010},
@@ -40,16 +55,18 @@ secondUser.education = [
     {name: "SMA 01", city: "Surabaya", graduate: 2016},
     {name: "Universitas Maju", city: "Tangerang"}
 ]
-secondUser.favoriteRestaurant = new Set([
-    "Tempura", "Bento", "Sushi", "Pancake", 
-    "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
-])
+const wendyFavResto = new Favresto(
+    [
+        "Tempura", "Bento", "Sushi", "Pancake", 
+        "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
+    ]
+)
+secondUser.favoriteRestaurant = wendyFavResto;
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
 users.push(firstUser);
 users.push(secondUser);
-
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
